524ba28b1cde10e1b5d4897f7a262e0ec5b68b06
alstn <tkops365@gmail.com>
2022-01-09 09:24:55 +0900

    다날라가서 복사한것

---
+++ src/controller/index.ts
@@ -0,0 +1,8 @@
+import express, { Router } from 'express';
+import schoolController from './school.controller';
+import tradeController from './trade.controller';
+const router = express.Router();
+
+router.use('/trade', tradeController);
+
+export default router;
